1b964a3504684cca87de2c20a87fa246264a176b
elenbokova <nansydrew@mail.ru>
2024-06-19 12:51:00 +0400

    Relocate icon

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ const syncServer = () => {
   gulp.watch('source/img/**/*.svg', gulp.series(copySvg, sprite, refresh));
   gulp.watch('source/img/**/*.{png,jpg,webp}', gulp.series(copyImages, refresh));
   gulp.watch('source/favicon/**', gulp.series(copy, refresh));
+  gulp.watch('source/**.ico', gulp.series(copy, refresh));
   gulp.watch('source/video/**', gulp.series(copy, refresh));
   gulp.watch('source/downloads/**', gulp.series(copy, refresh));
   gulp.watch('source/*.php', gulp.series(copy, refresh));
