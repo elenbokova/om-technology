e66ddbe72eebc18cbbb0aada591046bb687f602e
elenbokova <nansydrew@mail.ru>
2024-06-19 12:26:07 +0400

    Add manifest and relocate icon

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,20 @@ import del from 'del';
 import styles from './gulp/compileStyles.mjs';
 import { copy, copyImages, copySvg } from './gulp/copyAssets.mjs';
 import js from './gulp/compileScripts.mjs';
-import {optimizeSvg, sprite, createWebp, optimizePng, optimizeJpg} from './gulp/optimizeImages.mjs';
+import { optimizeSvg, sprite, createWebp, optimizePng, optimizeJpg } from './gulp/optimizeImages.mjs';
 
 const server = browserSync.create();
+
 const streamStyles = () => styles().pipe(server.stream());
 
 const clean = () => del('build');
+
+const copyManifest = () => gulp.src('source/manifest.webmanifest').pipe(gulp.dest('build'));
+
+const refresh = (done) => {
+  server.reload();
+  done();
+};
 
 const syncServer = () => {
   server.init({
@@ -26,20 +34,15 @@ const syncServer = () => {
   gulp.watch('source/data/**/*.{js,json}', gulp.series(copy, refresh));
   gulp.watch('source/img/**/*.svg', gulp.series(copySvg, sprite, refresh));
   gulp.watch('source/img/**/*.{png,jpg,webp}', gulp.series(copyImages, refresh));
-
   gulp.watch('source/favicon/**', gulp.series(copy, refresh));
   gulp.watch('source/video/**', gulp.series(copy, refresh));
   gulp.watch('source/downloads/**', gulp.series(copy, refresh));
   gulp.watch('source/*.php', gulp.series(copy, refresh));
+  gulp.watch('source/manifest.webmanifest', gulp.series(copyManifest, refresh));
 };
 
-const refresh = (done) => {
-  server.reload();
-  done();
-};
-
-const build = gulp.series(clean, copy, sprite, gulp.parallel(styles, js, optimizePng, optimizeJpg, optimizeSvg));
+const build = gulp.series(clean, copy, sprite, gulp.parallel(styles, js, optimizePng, optimizeJpg, optimizeSvg, copyManifest));
 const dev = gulp.series(clean, copy, sprite, gulp.parallel(styles, js, optimizePng, optimizeJpg, optimizeSvg), syncServer);
 const start = gulp.series(clean, copy, sprite, gulp.parallel(styles, js), syncServer);
 
-export { createWebp as webp, build, start, dev};
+export { createWebp as webp, build, start, dev };
